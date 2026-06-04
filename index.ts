fb8f4372eaddf021f706ef38263049fa217f5348
nguyenhaiphong0711 <nguyenhaiphong0711@gmail.com>
2026-06-04 23:49:16 +0700

    update something

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-import express from 'express'
+import express , {Request , Response } from 'express'
+import path from "path";
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', (req: Request, res: Response) => {
+  res.render("client/pages/home");
 })
-
+// Thiet lap file tinh cho project
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
